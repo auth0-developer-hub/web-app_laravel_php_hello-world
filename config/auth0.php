4605a7f1af6d32efbbc6ae5d26f8f17daba6adc2
Byron Motoche <116190812+byron-okta@users.noreply.github.com>
2024-01-17 19:39:15 -0500

    implement authentication with external api integration

--- config/auth0.php
+++ config/auth0.php
@@ -4,5 +4,6 @@ return [
     'domain' => env('AUTH0_DOMAIN'),
     'clientId' => env('AUTH0_CLIENT_ID'),
     'clientSecret' => env('AUTH0_CLIENT_SECRET'),
-    'cookieSecret' => env('APP_KEY')
+    'cookieSecret' => env('APP_KEY'),
+    'audience' => [env('AUTH0_AUDIENCE')]
 ];
